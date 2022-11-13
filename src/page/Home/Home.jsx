168499d2a619d121652f467e8ce9018622b11415
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-13 12:58:26 +0330

    add header

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../component/Header/Header";
+import Grid from "@mui/material/Grid";
+
+const Home = () => {
+    return(
+       <Grid>
+            <Header/>
+       </Grid> 
+    )
+}
+
+export default Home;

528f63974da3500a646cb0c2b7e255452b124c85
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-14 08:15:19 +0330

    change to import component

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "../../component/Header/Header";
+import Main from "../../component/Main/Main";
 import Grid from "@mui/material/Grid";
 
 const Home = () => {
+    
+    const [search, setSearch] = useState('');
+    const [open, setOpen] = useState(false);
+
     return(
        <Grid>
-            <Header/>
+            <Header setSearch={setSearch} setOpen={setOpen}/>
+            <Main search={search} open={open} setOpen={setOpen}/>
        </Grid> 
     )
 }

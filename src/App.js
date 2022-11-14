4c68a30cb95a39f1a9d328536713798d1dca4ece
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-14 08:16:57 +0330

    add store and provider

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 // import { RouterProvider } from 'react-router-dom';
 // import router from './route/route';
 import Home from './page/Home/Home';
+import {Provider} from 'react-redux';
+import {store} from "./component/redux/store";
 
 function App() {
   return (
-    <Home/>
+    <Provider store={store}>
+      <Home/>
+    </Provider>
   );
 }
 

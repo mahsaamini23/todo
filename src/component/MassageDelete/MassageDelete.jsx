85acb4c7bfff25e7ef95ce0b3da15a0e52013508
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-26 15:20:12 +0330

    create delete massage component

---
+++ src/component/MassageDelete/MassageDelete.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+const MassageDelete =()=>{
+    return(
+        <Grid>
+
+        </Grid>
+    )
+}
+
+export default MassageDelete;

8296dee38f42eeda3c3aae77112c8e2e8115cd0b
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-28 23:28:53 +0330

    add logic

--- src/component/CardTask/CardTask.jsx
+++ src/component/CardTask/CardTask.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {AddItem, DeleteItem, EditItem, CheckItem} from "../redux/todoReducer/todoReducer";
-import MyCalendar from "../Calendar/Calendar";
+import MassageDelete from "../MassageDelete/MassageDelete";
 import Box from "@mui/material/Box";
 import Card from '@mui/material/Card';
 import CardContent  from "@mui/material/CardContent";
@@ -12,46 +12,41 @@ import Typography from "@mui/material/Typography";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete" ;
 import DoneIcon from '@mui/icons-material/Done';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 
 
-const CardTask =({search, setForm, setMode})=>{
+const CardTask =({search, setForm, setMode,setOpen})=>{
 
-    const [ShowCalendar, setShowCalendar] = useState(false)
+    const [openDialog, setOpenDialog] = useState(false)
     const [color, setColor] = useState(false)
+    const [id , setId] = useState(0)
     const task = useSelector((state)=>state.todo);
     const dispatch = useDispatch();
 
-    const handleShowCalendar=()=>{
-        setShowCalendar(current => !current)
-    }
+    
 
     const handelEdit = (item) =>{
+        setOpen(true)
         dispatch(EditItem(item))
         setMode(false);
         setForm(item);
     }
     
-    const handelDelete = (id) =>{
-        dispatch(DeleteItem(id))
+    const handleShowMassageDelete =(id)=>{
+        setOpenDialog(true)
+        setId(id)
     }
 
     const handelCheck = (id)=>{
         dispatch(CheckItem(id))
-        setColor(dispatch(CheckItem(id)))
     }
- 
 
     return(
         <>
-        <Box sx={{ position:'absolute',top:'5px',zIndex:'10', right:'10px', flexDirection:'row-reverse'}} display={ShowCalendar ? 'flex' : 'none'}>
-            <MyCalendar/>
-        </Box>
         {task.filter(t => search === "" ? t : t.title.toLowerCase().includes(search.toLowerCase()))
         .map(task =>(
-            <Card key={task.id} sx={{width:'65%', boxShadow:'none', border:'1px solid #e0e0e0',margin:'5px 10px'}}>
-            <CardContent sx={{display:'flex',justifyContent:'space-between'}}>
+        <Card key={task.id} sx={{width:'80%', boxShadow:'none', border:'1px solid #e0e0e0',margin:'5px 10px'}}>
+            <CardContent sx={{display:'flex', flexDirection:{xs:'column', md:'row'},justifyContent:{md:'space-between'}, gap:{xs:'15px', md:'0'}}}>
                 <Grid sx={{display:'flex',gap:'10px'}}>
                     <CheckBox onClick={()=>handelCheck(task.id)}/>
                     <Grid sx={{display:'flex', flexDirection:'column',paddingTop:'10px'}}>
@@ -59,30 +54,26 @@ const CardTask =({search, setForm, setMode})=>{
                         <Typography sx={{fontSize:'12px'}}>{task.description}</Typography>
                     </Grid>
                 </Grid>
-                <Grid sx={{display:'flex'}}>
-                    <IconButton >{
+                <Grid sx={{display:'flex', justifyContent:{xs:'flex-end', md:''}}}>
+                    <Grid sx={{padding:{xs:'9px 6px 0 0',md:'13px 5px 0 0'}, color:'grey'}} >{
                         task.status ? 
                         (<DoneIcon color='success'/>)
-                         :(
-                            <DoneIcon color='grey'/>
-                        )}
-                    </IconButton>
+                        :
+                        (<DoneIcon color='grey'/>)
+                    }
+                    </Grid>
                     <IconButton onClick={()=>handelEdit(task)}>
                         <EditIcon/>
                     </IconButton>
-                    <IconButton onClick={()=>handelDelete(task.id)}>
+                    <IconButton onClick={()=>handleShowMassageDelete(task.id)}>
                         <DeleteIcon/>
-                    </IconButton>
-                    <IconButton onClick={handleShowCalendar}>
-                        <CalendarMonthIcon/>
                     </IconButton>
                 </Grid>
             </CardContent>
-            
+            <MassageDelete setOpenDialog={setOpenDialog} openDialog={openDialog} id={id}/>
         </Card>
         ))}
         </>
-        
     )
 }
 

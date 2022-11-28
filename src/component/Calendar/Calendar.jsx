0ecf8c003c6c549ba42c613d7a50566406c38f80
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-28 23:28:32 +0330

    add style

--- src/component/Calendar/Calendar.jsx
+++ src/component/Calendar/Calendar.jsx
@@ -1,23 +1,17 @@
-import React,{useState} from 'react';
+import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './calendar.css';
 
-const MyCalendar = ()=>{
-    const [date, setDate] = useState(new Date());
-    console.log(date);
-    const handleChange=(date)=>{
-        setDate(date)
-        // console.log(date);
-    }
-    const handleClickDay=()=>{
-        // console.log("ok")
-    }
+const MyCalendar = ({date, setDate})=>{
+    
+  const handleClickDay=(e)=>{
+    setDate(e)
+  }
 
   return (
     <div style={{marginTop:'60px'}}>
-        <Calendar onChange={handleChange} value={date} onClickDay={handleClickDay}/>
-        {/* <div>{date}</div> */}
+      <Calendar value={date} onChange={handleClickDay}/>
     </div>
   ); 
 }

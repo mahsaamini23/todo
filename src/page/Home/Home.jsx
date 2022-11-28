e73fc04be0fdc7361568c09e4aea650688601e11
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-28 23:30:26 +0330

    add state

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -6,12 +6,13 @@ import Grid from "@mui/material/Grid";
 const Home = () => {
     
     const [search, setSearch] = useState('');
+    const [ShowCalendar, setShowCalendar] = useState(false)
     const [open, setOpen] = useState(false);
 
     return(
        <Grid>
-            <Header setSearch={setSearch} setOpen={setOpen}/>
-            <Main search={search} open={open} setOpen={setOpen}/>
+            <Header setSearch={setSearch} setOpen={setOpen} setShowCalendar={setShowCalendar}/>
+            <Main search={search} open={open} setOpen={setOpen}  ShowCalendar={ShowCalendar}/>
        </Grid> 
     )
 }
